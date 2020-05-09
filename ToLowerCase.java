0fe4e5b2d9aeb6c7f0a89fbeca7399ec56118da4
Islombek Karimov <karimovislombek1994@gmail.com>
2020-05-09 20:28:02 +0300

    Add files via upload

---
+++ ToLowerCase.java
@@ -0,0 +1,14 @@
+package leetCode.algorithms;
+
+/**
+ * Created by Islombek Karimov on 09.05.2020.
+ */
+public class ToLowerCase {
+    public static void main(String[] args) {
+        System.out.println(toLowerCase("Hello"));
+    }
+
+    public static String toLowerCase(String str) {
+        return str.toLowerCase();
+    }
+}
