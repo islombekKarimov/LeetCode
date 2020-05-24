5d826fcc502f11e48f2ea147f9bd899f611ead30
Islombek Karimov <karimovislombek1994@gmail.com>
2020-05-24 19:29:04 +0300

    Add files via upload

---
+++ NumberStudentsDoingHomeworkGivenTime.java
@@ -0,0 +1,24 @@
+package leetCode.algorithms;
+
+/**
+ * Created by Islombek Karimov on 23.05.2020.
+ */
+public class NumberStudentsDoingHomeworkGivenTime {
+
+    public static void main(String[] args) {
+        System.out.println(busyStudent(new int[]{4}, new int[]{4}, 5));
+
+    }
+
+
+    public static int busyStudent(int[] startTime, int[] endTime, int queryTime) {
+        int count = 0;
+        for (int i = 0; i < startTime.length; i++) {
+            if (startTime[i] <= queryTime && endTime[i]>=queryTime) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
