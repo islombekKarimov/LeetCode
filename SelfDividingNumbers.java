ad99a242c8b9c291761600931a3c191bda4d4ddd
Islombek Karimov <karimovislombek1994@gmail.com>
2020-06-11 14:55:51 +0300

    Add files via upload

---
+++ SelfDividingNumbers.java
@@ -0,0 +1,39 @@
+package leetCode.algorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Islombek Karimov on 11.06.2020.
+ */
+public class SelfDividingNumbers {
+    public static void main(String[] args) {
+        selfDividingNumbers(1, 22);
+    }
+
+
+    public static List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> integerList = new ArrayList<>();
+        for (int i = left; i <= right; i++) {
+                if (checkNumber(i)) {
+                    integerList.add(i);
+                }
+        }
+        integerList.stream().forEach(System.out::println);
+        return integerList;
+    }
+
+    public static boolean checkNumber(int num) {
+        int copy = num;
+        while (copy > 0) {
+            int digit = copy % 10;
+            if (digit == 0 || num % digit != 0) {
+                return false;
+            }
+            copy /= 10;
+        }
+        return true;
+    }
+
+
+}
