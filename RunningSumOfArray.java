a7c9819fef4a1ac627d3dcc0b5dd223dfd5265c6
Islombek Karimov <karimovislombek1994@gmail.com>
2020-07-08 20:57:29 +0300

    Add files via upload

---
+++ RunningSumOfArray.java
@@ -0,0 +1,21 @@
+package leetCode.algorithms;
+
+/**
+ * Created by Islombek Karimov on 06.07.2020.
+ */
+public class RunningSumOfArray {
+
+    public static void main(String[] args) {
+        runningSum(new int[]{1, 2, 3, 4});
+    }
+
+    public static int[] runningSum(int[] nums) {
+        int[] sum = new int[nums.length];
+        for (int i = 1; i < nums.length; i++) {
+                nums[i] += nums[i-1];
+            }
+
+        return nums;
+    }
+
+}
