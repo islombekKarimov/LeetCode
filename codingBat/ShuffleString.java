5e7e422cdbff2ea3e61f46725db953e20693c4da
@karimovislombek1994 <karimovislombek1994@gmail.com>
2020-08-12 20:42:12 +0500

    LeetCode

---
+++ codingBat/ShuffleString.java
@@ -0,0 +1,22 @@
+package LeetCode.codingBat;
+
+/**
+ * ShuffleString.
+ *
+ * @author Islombek Karimov
+ * @since 12.08.2020
+ */
+public class ShuffleString {
+    public static void main(String[] args) {
+        System.out.println(restoreString("codeleet", new int[]{4,5,6,7,0,2,1,3}));
+    }
+
+    public static String restoreString(String s, int[] indices) {
+        char[] mass = s.toCharArray();
+        for (int i = 0; i < mass.length; i++) {
+            mass[indices[i]] = s.charAt(i);
+        }
+        return String.valueOf(mass);
+    }
+
+}
