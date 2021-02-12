ecffc062435b6e3c3b4c03dcfd698668f9d1f330
Islombek Karimov <karimovislombek1994@gmail.com>
2021-02-12 09:56:15 +0500

    Add files via upload

---
+++ algorithms/CheckIfExit.java
@@ -0,0 +1,18 @@
+package com.leetCode;
+
+public class CheckIfExit {
+  public static void main(String[] args) {
+    System.out.println(checkIfExist(new int[] {7, 1, 14, 11}));
+  }
+
+  public static boolean checkIfExist(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = i + 1; j < arr.length; j++) {
+        if (arr[i] == arr[j] * 2 || arr[j] == arr[i] * 2) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+}
